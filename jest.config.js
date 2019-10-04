424313ff50ce51217e3995df70f08fa8ace9a39b
drafter500 <vzemtsov@yahoo.com>
2019-10-04 16:22:27 +0200

    CS-2054 - Fixes review comments

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,15 @@
 module.exports = {
-  "testURL": "https://caseable.com/de/de/someUrl",
-  "setupFilesAfterEnv": [
-    "./src/setupTests.js"
+  setupFilesAfterEnv: [
+    './src/setupTests.js',
   ],
-  "moduleNameMapper": {
-    "\\.(jpg|jpeg|png)$": "<rootDir>/__mocks__/fileMock.js",
-    "^.+\\.(css|scss)$": "identity-obj-proxy",
-    "\\.svg$": "<rootDir>/__mocks__/svgr-mock.js"
-  },
-  "testPathIgnorePatterns": [
-    "/node_modules/"
+  testPathIgnorePatterns: [
+    '/node_modules/',
   ],
-  "testEnvironmentOptions": {
-    "url": "https://caseable.com/de/de/someUrl"
-  },
-  "coverageDirectory": "./coverage",
-  "coverageReporters": [
-    "lcov"
+  coverageDirectory: './coverage',
+  coverageReporters: [
+    'lcov',
   ],
-  "collectCoverageFrom": [
-    "src/**/*.{js,jsx}"
-  ]
+  collectCoverageFrom: [
+    'src/**/*.js',
+  ],
 };
