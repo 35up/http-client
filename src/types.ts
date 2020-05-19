5eeeef843395fb18b03859840edf195f0a16d4a8
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2020-05-19 15:09:02 +0200

    HOTFIX - Fixes method args and response type

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,3 @@
 export type THeaders = Record<string, string>;
 
 export type TSearchParams = Record<string, string | string[]>;
-
-export type TJson = boolean | number | string | null | TJsonArray | TJsonMap;
-
-export type TJsonMap = { [key: string]: TJson };
-
-export type TJsonArray = Array<TJson>;
