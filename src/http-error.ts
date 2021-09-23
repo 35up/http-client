9549e60724dd3f1f80ab236601602e635337e440
drafter500 <vzemtsov@yahoo.com>
2021-09-23 13:54:13 +0200

    UP-2176 - Supports error as an object

--- src/http-error.ts
+++ src/http-error.ts
@@ -1,7 +1,14 @@
+function getMessage(data: any): string {
+  const error = data && data.error;
+  if (error && typeof error === 'object') {
+    return error.message;
+  }
+
+  return error;
+}
+
 export class HttpError extends Error {
   constructor(public data: any, public response: Response) {
-    super(
-      (data && data.error) || `${response.status} ${response.statusText}`,
-    );
+    super(getMessage(data) || `${response.status} ${response.statusText}`);
   }
 }
