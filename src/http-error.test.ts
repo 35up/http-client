e401b4ef3c68e0145a8a6e7d446509260b489d99
drafter500 <vzemtsov@yahoo.com>
2021-09-24 11:59:16 +0200

    UP-2176 - Adds space

--- src/http-error.test.ts
+++ src/http-error.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { HttpError } from './http-error';
 
+
 describe('http-error', () => {
   describe('toString', () => {
     it('to string returns error message', () => {
