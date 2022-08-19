c464beebc38826e06366a0d96fd0b5db82f0f062
gustavoaz7 <gustavo_azevedo_7@hotmail.com>
2022-08-19 10:34:59 +0200

    UP-3456 - Updates eslint

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,6 +1,7 @@
 import fetch, { Headers, Request, Response } from 'node-fetch';
 
 if (!globalThis.fetch) {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   Object.assign(globalThis, { fetch, Headers, Request, Response });
 }
 
