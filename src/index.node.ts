071be5201892b8086b9292577a708beec56441c5
Fausto Arturo Guerrero Velez <enjoythelive1@gmail.com>
2022-09-22 15:46:27 +0200

    UP-3447 - Fixes types for node

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,8 +1,27 @@
-import fetch, { Headers, Request, Response } from 'node-fetch';
+/* eslint-disable @typescript-eslint/ban-types, no-var, vars-on-top */
+/* eslint-disable @typescript-eslint/naming-convention */
+import importedFetch, {
+  Headers,
+  Request,
+  Response as ImportedResponse,
+  RequestMode as ImportedRequestMode,
+} from 'node-fetch';
+
+declare global {
+  // @ts-ignore we dont have separate tsconfig for node and browser, but
+  // importers of the library are not using browser symbols, so this is
+  // needed for them
+  var fetch: typeof importedFetch;
+  type Response = ImportedResponse;
+  type RequestMode = ImportedRequestMode;
+  type FormData = unknown;
+}
 
 if (!globalThis.fetch) {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
-  Object.assign(globalThis, { fetch, Headers, Request, Response });
+  globalThis.fetch = importedFetch;
+  globalThis.Headers = Headers;
+  globalThis.Request = Request;
+  globalThis.Response = ImportedResponse;
 }
 
 export * from './index';
