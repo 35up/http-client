2f133b53f785910f00314c6d669d118cda03b70a
Fausto Arturo Guerrero Velez <enjoythelive1@gmail.com>
2022-11-01 15:33:59 +0100

    UP-3509 - Fixes ts errors on node projects

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,27 +1,14 @@
-/* eslint-disable @typescript-eslint/ban-types, no-var, vars-on-top */
-/* eslint-disable @typescript-eslint/naming-convention */
 import importedFetch, {
   Headers,
   Request,
-  Response as ImportedResponse,
-  RequestMode as ImportedRequestMode,
+  Response,
 } from 'node-fetch';
-
-declare global {
-  // @ts-ignore we dont have separate tsconfig for node and browser, but
-  // importers of the library are not using browser symbols, so this is
-  // needed for them
-  var fetch: typeof importedFetch;
-  type Response = ImportedResponse;
-  type RequestMode = ImportedRequestMode;
-  type FormData = unknown;
-}
 
 if (!globalThis.fetch) {
   globalThis.fetch = importedFetch;
   globalThis.Headers = Headers;
   globalThis.Request = Request;
-  globalThis.Response = ImportedResponse;
+  globalThis.Response = Response;
 }
 
 export * from './index';
